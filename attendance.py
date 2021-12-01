a5faee1b038a045fc5edfb2bbf79988b43914b04
Vidya Sharma <63513761+sharma021@users.noreply.github.com>
2021-12-01 06:17:06 +0530

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -26,12 +26,21 @@ class Attendance:
         f_lbl.place(x=600,y=0,width=800,height=200)
 
 #background image
-        img_bg=Image.open(r"F:\projects\ml\college_images\face_detector1.jpg")
-        img_bg=img_bg.resize((1530,700),Image.ANTIALIAS)
-        self.photoimg_bg=ImageTk.PhotoImage(img_bg)
-        bg_img=Label(self.root,image=self.photoimg_bg)
+        img3=Image.open(r"F:\projects\ml\college_images\face_detector1.jpg")
+        img3=img3.resize((1530,700),Image.ANTIALIAS)
+        self.photoimg3=ImageTk.PhotoImage(img3)
+        bg_img=Label(self.root,image=self.photoimg3)
         bg_img.place(x=0,y=200,width=1530,height=700)
 
+        title_lbl=Label(bg_img,text="ATTENDANCE MANAGEMENT SYSTEM", font=("times new roman",35,"bold"),bg="white" ,anchor="center", fg="green")
+        title_lbl.place(x=0,y=0,width=1530,height=35)
+
+#main FRAME
+        main_frame=Frame(bg_img,bd=2,bg="white")
+        main_frame.place(x=0,y=45,width=1450,height=600)
+
+        Left_frame=LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="Student Details",font=("times new roman",12,"bold"))
+        Left_frame.place(x=10,y=10,width=660,height=500)
 
 
 if __name__=='__main__':
