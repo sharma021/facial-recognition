9af110633f06469d344dd34b8a915d9f37c50958
Vidya Sharma <63513761+sharma021@users.noreply.github.com>
2021-12-07 06:52:38 +0530

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -4,13 +4,25 @@ from PIL import Image,ImageTk
 from tkinter import messagebox
 import mysql.connector
 import cv2
-
-
+import os
+import csv
+from tkinter import filedialog
+mydata=[]
 class Attendance:
     def __init__(self,root):
         self.root=root
         self.root.geometry("1500x790+0+0")
         self.root.title("Face Recognition System")
+
+
+#---------------------------------------Variables-----------------------------------#
+        self.var_atten_id=StringVar()
+        self.var_atten_roll=StringVar()
+        self.var_atten_name=StringVar()
+        self.var_atten_dep=StringVar()
+        self.var_atten_time=StringVar()
+        self.var_atten_date=StringVar()
+        self.var_atten_attendance=StringVar()
 #first Image
         img_top=Image.open(r"F:\projects\ml\college_images\smart-attendance.jpg")
         img_top=img_top.resize((800,200),Image.ANTIALIAS)
@@ -57,35 +69,44 @@ class Attendance:
         attendanceId_label=Label(left_inside_frame,relief=RIDGE,text="Student ID", font=("conicsansns 11 bold"),bg="white")
         attendanceId_label.grid(row=0,column=0,padx=10,pady=5,sticky=W)
 
-        attendanceId_entry=Entry(left_inside_frame,width=20,font=("conicsansns 11 bold"))
+        attendanceId_entry=Entry(left_inside_frame,width=20,textvariable=self.var_atten_id,font=("conicsansns 11 bold"))
         attendanceId_entry.grid(row=0,column=1,padx=10,pady=5,sticky=W)
 
         #Roll lABEL
         Roll_label=Label(left_inside_frame,relief=RIDGE,text="Roll No", font=("conicsansns 11 bold"),bg="white")
         Roll_label.grid(row=0,column=2,padx=4,pady=8,sticky=W)
 
-        atten_label_entry=Entry(left_inside_frame,width=20,font=("conicsansns 11 bold"))
+        atten_label_entry=Entry(left_inside_frame,width=20,textvariable=self.var_atten_roll,font=("conicsansns 11 bold"))
         atten_label_entry.grid(row=0,column=3,padx=8,pady=5,sticky=W)
 
         #Name
         name_label=Label(left_inside_frame,relief=RIDGE,text="Name", font=("conicsansns 11 bold"),bg="white")
         name_label.grid(row=1,column=0,padx=10,pady=5,sticky=W)
 
-        attten_name_entry=Entry(left_inside_frame,width=20,font=("conicsansns 11 bold"))
+        attten_name_entry=Entry(left_inside_frame,width=20,textvariable=self.var_atten_name,font=("conicsansns 11 bold"))
         attten_name_entry.grid(row=1,column=1,padx=10,pady=5,sticky=W)
 
-        #Date
+        #Department
         dep_label=Label(left_inside_frame,relief=RIDGE,text="Department", font=("conicsansns 11 bold"),bg="white")
         dep_label.grid(row=1,column=2,padx=10,pady=5,sticky=W)
 
-        depl_entry=Entry(left_inside_frame,width=20,font=("conicsansns 11 bold"))
+        depl_entry=Entry(left_inside_frame,width=20,textvariable=self.var_atten_dep,font=("conicsansns 11 bold"))
         depl_entry.grid(row=1,column=3,padx=10,pady=5,sticky=W)
 
-        time_label=Label(left_inside_frame,relief=RIDGE,text="Time", font=("conicsansns 11 bold"),bg="white")
-        time_label.grid(row=2,column=0,padx=10,pady=5,sticky=W)
+#Date
+        dep_label=Label(left_inside_frame,relief=RIDGE,text="Date", font=("conicsansns 11 bold"),bg="white")
+        dep_label.grid(row=2,column=0,padx=10,pady=5,sticky=W)
 
-        T_entry=Entry(left_inside_frame,width=20,font=("conicsansns 11 bold"))
-        T_entry.grid(row=2,column=1,padx=10,pady=5,sticky=W)
+        depl_entry=Entry(left_inside_frame,width=20,textvariable=self.var_atten_date,font=("conicsansns 11 bold"))
+        depl_entry.grid(row=2,column=1,padx=10,pady=5,sticky=W)
+
+
+#Time
+        time_label=Label(left_inside_frame,relief=RIDGE,text="Time", font=("conicsansns 11 bold"),bg="white")
+        time_label.grid(row=2,column=2,padx=10,pady=5,sticky=W)
+
+        T_entry=Entry(left_inside_frame,width=20,textvariable=self.var_atten_time,font=("conicsansns 11 bold"))
+        T_entry.grid(row=2,column=3,padx=10,pady=5,sticky=W)
 
         #attendance PhotoSampleStatus
         attendancelabel=Label(left_inside_frame,text="Attendance Status",bg="white",font="comicsansns 11 bold")
@@ -99,16 +120,16 @@ class Attendance:
         btn_frame=Frame(left_inside_frame,bd=2,relief=RIDGE,bg="white")
         btn_frame.place(x=0,y=200,width=650,height=60)
 
-        save_btn=Button(btn_frame,text="Import csv",width=15,font=("times new roman",13,"bold"),bg="blue",fg="white")
+        save_btn=Button(btn_frame,text="Import csv",command=self.importCsv,width=15,font=("times new roman",13,"bold"),bg="blue",fg="white")
         save_btn.grid(row=0,column=0)
 
-        update_btn=Button(btn_frame,text="Export csv",width=15,font=("times new roman",13,"bold"),bg="blue",fg="white")
+        update_btn=Button(btn_frame,text="Export csv",command=self.exportCsv,width=15,font=("times new roman",13,"bold"),bg="blue",fg="white")
         update_btn.grid(row=0,column=1)
 
         delete_btn=Button(btn_frame,text="Update",width=15,font=("times new roman",13,"bold"),bg="blue",fg="white")
         delete_btn.grid(row=0,column=2)
 
-        reset_btn=Button(btn_frame,text="Reset",width=15,font=("times new roman",13,"bold"),bg="blue",fg="white")
+        reset_btn=Button(btn_frame,text="Reset",width=15,command=self.reset_data,font=("times new roman",13,"bold"),bg="blue",fg="white")
         reset_btn.grid(row=0,column=3)
 
         #right label frame
@@ -150,9 +171,59 @@ class Attendance:
 
 
         self.AttendanceReportTable.pack(fill=BOTH,expand=1)
+        self.AttendanceReportTable.bind("<ButtonRelease>",self.get_cursor)
+#-----------------------------------------------FETCH DATA-----------------------------------------#
+    def fetchData(self,rows):
+        self.AttendanceReportTable.delete(*self.AttendanceReportTable.get_children())
+        for i in rows:
+            self.AttendanceReportTable.insert("",END,values=i)
 
+#import csv
+    def importCsv(self):
+        global mydata
+        mydata.clear()
+        fln=filedialog.askopenfilename(initialdir=os.getcwd(),title="Open CSV",filetypes=(("CSV File","*.csv"),("ALL File","*.*")),parent=self.root)
+        with open(fln) as myfile:
+            csvread=csv.reader(myfile,delimiter=",")
+            for i in csvread:
+                mydata.append(i)
+            self.fetchData(mydata)
 
+#export csvread
+    def exportCsv(self):
+        try:
+            if len(mydata)<1:
+                messagebox.showerror("No Data","No Data found to export",parent=self.root)
+                return False
+            fln=filedialog.asksaveasfilename(initialdir=os.getcwd(),title="Open CSV",filetypes=(("CSV File","*.csv"),("ALL File","*.*")),parent=self.root)
+            with open(fln,mode="w",newline="") as myfile:
+                exp_write=csv.writer(myfile,delimiter=",")
+                for i in mydata:
+                    exp_write.writerow(i)
+                messagebox.showinfo("Data Export","Your data exported to"+os.path.basename(fln)+"successfully")
+        except Exception as es:
+            messagebox.showerror("Erro",f"Due to :{str(es)}",parent=self.root)
 
+    def get_cursor(self,event=""):
+        cursor_row=self.AttendanceReportTable.focus()
+        content=self.AttendanceReportTable.item(cursor_row)
+        rows=content['values']
+        self.var_atten_id.set(rows[0])
+        self.var_atten_roll.set(rows[1])
+        self.var_atten_name.set(rows[2])
+        self.var_atten_dep.set(rows[3])
+        self.var_atten_time.set(rows[4])
+        self.var_atten_date.set(rows[5])
+        self.var_atten_attendance.set(rows[6])
+
+    def reset_data(self):
+        self.var_atten_id.set("")
+        self.var_atten_roll.set("")
+        self.var_atten_name.set("")
+        self.var_atten_dep.set("")
+        self.var_atten_time.set("")
+        self.var_atten_date.set("")
+        self.var_atten_attendance.set("")
 
 
 
