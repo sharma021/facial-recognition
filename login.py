f8ac8aba772bd119f816725414f9df7e335347f4
Vidya Sharma <63513761+sharma021@users.noreply.github.com>
2021-12-08 18:01:38 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,6 +1,7 @@
 from tkinter import*
 from tkinter import ttk
 from PIL import Image,ImageTk
+from tkinter import messagebox
 
 
 class Login_Window:
@@ -35,8 +36,8 @@ class Login_Window:
         password=lbl=Label(frame,text="PASSWORD",font=("times new roman",15,"bold"),fg="white",bg="black")
         password.place(x=100,y=225)
 
-        self.txtuser=ttk.Entry(frame,font=("times new roman",15,"bold"))
-        self.txtuser.place(x=40,y=250,width=270)
+        self.txtpass=ttk.Entry(frame,font=("times new roman",15,"bold"))
+        self.txtpass.place(x=40,y=250,width=270)
 
         #-------------------Icon Images------------------#
         img2=Image.open(r"F:\projects\ml\college_images\di.jpg")
@@ -51,7 +52,25 @@ class Login_Window:
         lblimg1=Label(image=self.photoimage3,bg="black",borderwidth=0)
         lblimg1.place(x=580,y=393,width=25,height=25)
 
+        #LOGIN BUTTON
+        loginbtn=Button(frame,command=self.login,text="Login",font=("times new roman",15,"bold"),bd=3,relief=RIDGE,fg="white",bg="red",activeforeground="white",activebackground="red")
+        loginbtn.place(x=120,y=300,width=120,height=35)
 
+        #Register Button
+        registerbtn=Button(frame,text="New User Register",font=("times new roman",10,"bold"),borderwidth=0,relief=RIDGE,fg="white",bg="black",activeforeground="white",activebackground="red")
+        registerbtn.place(x=20,y=350,width=120)
+
+        #Forget password Button
+        forgetpassbtn=Button(frame,text="Forget password",font=("times new roman",10,"bold"),borderwidth=0,relief=RIDGE,fg="white",bg="black",activeforeground="white",activebackground="red")
+        forgetpassbtn.place(x=20,y=370,width=120)
+
+    def login(self):
+        if self.txtuser.get()=="" or self.txtpass.get()=="":
+            messagebox.showerror("Error!","All Fields are Required!")
+        elif self.txtuser.get()=="vidu" and self.txtpass.get()=="anshu":
+            messagebox.showinfo("Success!","Welcome To V Snap!!")
+        else:
+            messagebox.showerror("Invalid","Invalid username and password!")
 
 
 
