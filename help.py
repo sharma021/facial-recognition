b3ff398baa04b9a23585c4e6e96c5ad76b7a8d41
Vidya Sharma <63513761+sharma021@users.noreply.github.com>
2021-12-08 06:41:57 +0530

    Update help.py

--- help.py
+++ help.py
@@ -1,1 +1,31 @@
+from tkinter import*
+from tkinter import ttk
+from PIL import Image,ImageTk
+from tkinter import messagebox
+import mysql.connector
+import cv2
 
+class Help:
+    def __init__(self,root):
+        self.root=root
+        self.root.geometry("1500x790+0+0")
+        self.root.title("Face Recognition System")
+
+        title_lbl=Label(self.root,text="Your Problem, Our Problem!", font=("times new roman",20,"bold"),bg="white" ,anchor="center", fg="red")
+        title_lbl.place(x=0,y=0,width=1530,height=35)
+
+#Top Image
+        img_top=Image.open(r"F:\projects\ml\college_images\iStock-1163542789-945x630.jpg")
+        img_top=img_top.resize((1400,600),Image.ANTIALIAS)
+        self.photoimg_top=ImageTk.PhotoImage(img_top)
+        f_lbl=Label(self.root,image=self.photoimg_top)
+        f_lbl.place(x=0,y=50,width=1400,height=600)
+
+        
+
+
+
+if __name__=='__main__':
+    root=Tk()
+    obj=Help(root)
+    root.mainloop()
