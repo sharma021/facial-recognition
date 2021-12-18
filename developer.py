b88b8218edb364a6abad3e8f966803b6fe2f7f33
Vidya Sharma <63513761+sharma021@users.noreply.github.com>
2021-12-18 20:12:09 +0530

    Update developer.py

--- developer.py
+++ developer.py
@@ -1,10 +1,10 @@
 from tkinter import*
 from tkinter import ttk
+
 from PIL import Image,ImageTk
 from tkinter import messagebox
 import mysql.connector
 import cv2
-
 
 class Developer:
     def __init__(self,root):
@@ -18,7 +18,6 @@ class Developer:
         Back_Button=Button(self.root,text="Back",command=self.root.destroy,font=("arial",11,"bold"),width=17,bg="white",fg="red")
         Back_Button.pack(side=LEFT)
         Back_Button.place(x=0,y=20)
-
 #Top Image
         img_top=Image.open(r"F:\projects\ml\college_images\dev.jpg")
         img_top=img_top.resize((1400,600),Image.ANTIALIAS)
@@ -30,8 +29,8 @@ class Developer:
         main_frame=Frame(f_lbl,bd=2,bg="white")
         main_frame.place(x=902,y=0,width=450,height=500)
 
-        img_top1=Image.open(r"F:\projects\ml\college_images\1_5TRuG7tG0KrZJXKoFtHlSg.jpeg")
-        img_top1=img_top1.resize((200,300),Image.ANTIALIAS)
+        img_top1=Image.open(r"F:\projects\ml\facial recognition system\file photos\#DevFestIndia.png")
+        img_top1=img_top1.resize((200,150),Image.ANTIALIAS)
         self.photoimg_top1=ImageTk.PhotoImage(img_top1)
 
         f_lbl=Label(main_frame,image=self.photoimg_top1)
@@ -41,15 +40,27 @@ class Developer:
         dev_label=Label(main_frame,text="Hello! I am Vidya Sharma", font=("times new roman",13,"bold"),bg="white")
         dev_label.place(x=1,y=5)
 
-        dev_label=Label(main_frame,text="I am an Information Technology Student", font=("times new roman",13,"bold"),bg="white")
+        dev_label=Label(main_frame,text="I am the designer of V SNAP!", font=("times new roman",13,"bold"),bg="white")
         dev_label.place(x=0,y=30)
 
+        dev_label=Label(main_frame,text="ABOUT V SNAP:", font=("times new roman",15,"bold"),bg="white",fg="red")
+        dev_label.place(x=2,y=60)
+
+        dev_label=Label(main_frame,text="V SNAP, aims to lower your burden\n and give you modern solution in the pandemic times!",font=("times new roman",10,"bold"),fg="black")
+        dev_label.place(x=2,y=100)
+
+        dev_label=Label(main_frame,text="Our project aims into storing attendance completely online\n and following the rules of maintaing a social distance.\n For this project candidate just need to mark their attendance\n not by a sign, not by a thumb print\n rather just by standing in front of a camera without touching anything!!",font=("times new roman",10,"bold"),fg="black")
+        dev_label.place(x=2,y=150)
+
+
         #Image
-        img2=Image.open(r"F:\projects\ml\college_images\Stanford.jpg")
-        img2=img2.resize((500,300),Image.ANTIALIAS)
+        img2=Image.open(r"F:\projects\ml\facial recognition system\file photos\logo.png")
+        img2=img2.resize((420,300),Image.ANTIALIAS)
         self.photoimg2=ImageTk.PhotoImage(img2)
         f_lbl=Label(main_frame,image=self.photoimg2)
-        f_lbl.place(x=0,y=200,width=500,height=300)
+        f_lbl.place(x=0,y=250,width=480,height=280)
+
+
 
 
 
